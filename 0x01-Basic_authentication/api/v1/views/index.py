d9951ab16d509b5b85b6f3a4e11dd9e3e37cba61
M1r1c1e <andersonmiracle@gmail.com>
2024-08-07 21:04:28 +0000

    basic authentication

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -13,6 +13,14 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+@app_views.route('/Unauthorized', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    abort(401)
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
