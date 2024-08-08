6aed9ab6725589d459ce1808a323957d068ab286
M1r1c1e <andersonmiracle@gmail.com>
2024-08-08 23:02:29 +0000

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,36 +8,47 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = os.getenv("AUTH_TYPE")
+AUTH_TYPE = os.getenv("AUTH_TYPE", "auth")
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-if AUTH_TYPE == 'basic_auth':
+if AUTH_TYPE == "basic_auth":
     auth = BasicAuth()
+if AUTH_TYPE == "session_auth":
+    auth = SessionAuth()
+if AUTH_TYPE == "session_exp_auth":
+    auth = SessionExpAuth()
+if AUTH_TYPE == "session_db_auth":
+    auth = SessionDBAuth()
 
 
 @app.before_request
 def before_request():
-    """ filter request """
+    """ Handles requests filtering """
     if auth:
         excluded_paths = [
                 '/api/v1/status/',
                 '/api/v1/unauthorized/',
-                '/api/v1/forbidden/'
+                '/api/v1/forbidden/',
+                '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
             user = auth.current_user(request)
-            if auth_header is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
                 abort(403)
+            request.current_user = user
 
 
 @app.errorhandler(404)
